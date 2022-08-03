27ff56088f7e0a78c7732fc70065e700348320c3
Neel Thomas <neeltom92@gmail.com>
2022-08-03 13:02:50 +0530

    Update main.go

--- main.go
+++ main.go
@@ -45,5 +45,5 @@ func sliCalculation(apiEndpoint string) {
 }
 
 func main() {
-	sliCalculation("https://api.example.com") // replace the endpoint with actual endpoint
+	sliCalculation("https://api.example.com/health") // replace the endpoint with actual endpoint
 }
